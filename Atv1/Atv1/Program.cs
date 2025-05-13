03577f9cb74f5197bfa60d8f23fb6abd54f6e8aa
Igor <igor.a@hotmail.com>
2025-05-13 13:55:30 -0300

    Adicionando Atv1

---
+++ Atv1/Atv1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Atv1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int indice = 13, soma = 0, k = 0;
+
+            while (k < indice)
+            {
+                k++;
+                soma += k;
+            }
+
+            Console.WriteLine("Soma: " + soma);
+        }
+    }
+}
