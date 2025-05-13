5e176b15bda25657ac16e1f0b8fada14fa469566
Igor <igor.a@hotmail.com>
2025-05-13 14:55:54 -0300

    Adicionando Atv5

---
+++ Atv5/Atv5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Atv5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Informe uma palavra ou frase para obter seu inverso: ");
+            string texto = Console.ReadLine();
+            string inversoTexto = "";
+
+            for(int i = texto.Length - 1; i >= 0; i--)
+            {
+                inversoTexto += texto[i];
+            }
+
+            Console.WriteLine($"O inverso de {texto} é {inversoTexto}");
+        }
+    }
+}
